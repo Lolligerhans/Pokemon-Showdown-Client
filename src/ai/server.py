16893648c2b5a108b455d143c96a0798dfd8d360
Lolligerhans <billadresse@gmx.de>
2019-04-26 05:21:05 +0200

    adjustments to test server

--- src/ai/server.py
+++ src/ai/server.py
@@ -9,21 +9,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def output():
-	# serve index template
-	return render_template('index.html', name='Joe')
+    # serve index template
+    return render_template('index.html', name='Joe')
 
 @app.route('/receiver', methods = ['POST'])
 def worker():
-	# read json + reply
-	data = request.get_json(True, False, False)
-	result = ''
+    # read json + reply
+    data = request.get_json(True, False, False)
+    result = ''
 
-	for item in data:
-		# loop over every row
-		result += str(item['make']) + '\n'
+    # loop over every row
+    for item in data:
+        make = str(item['make'])
+        if(make == 'Porsche'):
+            result += make + ' -- That is a good manufacturer\n'
+        else:
+            result += make + ' -- That is only an average manufacturer\n'
 
-	return result
+    return result
 
 if __name__ == '__main__':
-	# run!
-	app.run()
+    # run!
+    app.run()
